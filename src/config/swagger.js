f3c97f16123bbe65ebe2a66dd323e88d9593bb5d
shaheedwajee <shaheedwajee@gmail.com>
2026-03-21 22:26:55 +0530

    added port number too sawgger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -24,7 +24,7 @@ const options = {
         description: 'AWS EC2 Production Server',
         variables: {
           'ec2-public-ip': {
-            default: '98.81.229.185',
+            default: '98.81.229.185:3000',
             description: 'EC2 public IP address',
           },
         },
